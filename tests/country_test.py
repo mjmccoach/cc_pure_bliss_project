8517f5ec489390673ced8729906e1ce2895a43c5
mjmccoach <mmccoach88@gmail.com>
2020-10-02 19:19:41 +0100

    Started tests on Country class. country_has_name test working.

--- tests/country_test.py
+++ tests/country_test.py
@@ -6,4 +6,7 @@ class TestCountry(unittest.TestCase):
 
     def setUp(self):
         self.country1 = Country("Japan", "Asia", id)
+    
+    def test_country_has_name(self):
+        self.assertEqual("Japan", self.country1.name)
 
