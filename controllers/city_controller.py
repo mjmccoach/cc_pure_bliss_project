b043663ba6aa5ed4ae9ac9423e7ff3bf87ecb996
mjmccoach <mmccoach88@gmail.com>
2020-10-04 21:10:18 +0100

    Functionality to edit cities created. No button browser side to navigate, fix this next

--- controllers/city_controller.py
+++ controllers/city_controller.py
@@ -34,3 +34,22 @@ def add_city():
 def delete_city(id):
     city_repository.delete(id)
     return redirect ("/cities")
+
+
+@cities_blueprint.route("/cities/<id>/edit", methods = ["GET"])
+def edit_city(id):
+    city = city_repository.select(id)
+    countries = country_repository.select_all()
+    return render_template("cities/edit.html", city = city, countries = countries)
+
+@cities_blueprint.route("/cities/<id>", methods =["POST"])
+def update_city(id):
+    name = request.form["name"]
+    country_id = request.form["country_id"]
+    visited = request.form["visited"]
+
+    country = country_repository.select(country_id)
+    city = City(name, country_id, visited)
+
+    city_repository.update(city)
+    return redirect("/cities")
